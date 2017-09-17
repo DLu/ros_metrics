c9753c7a1141ad3e78990de69bc4be102fd63b91
David V. Lu <davidvlu@gmail.com>
2017-09-17 17:28:37 -0400

    Cleanup

--- plot.py
+++ plot.py
@@ -1,17 +1,16 @@
 import csv
 from matplotlib.pyplot import plot, show, legend, title
-from dateutil import parser # pip install python-dateutil
-import collections
+from dateutil import parser  # pip install python-dateutil
 
 PLOTS = [
-  ('Website Metrics', ['Homepage Views', 'Installation Views', 'Tutorials Views']),
-  ('Website by Country', ['US Views', 'China Views', 'Germany Views', 'Japan Views']),
-  ('ROS Answers Website Metrics', ['questions hits', 'ask hits']),
-  ('Communication Platforms', ['users subscribers', 'wiki.ros.org users', 'answers users', 'discourse users']),
-  ('Wiki Stats', ['wiki pages', 'wiki page views']),
-  ('ROS Answers Stats', ['Questions', 'Answered Questions']),
+    ('Website Metrics', ['Homepage Views', 'Installation Views', 'Tutorials Views']),
+    ('Website by Country', ['US Views', 'China Views', 'Germany Views', 'Japan Views']),
+    ('ROS Answers Website Metrics', ['questions hits', 'ask hits']),
+    ('Communication Platforms', ['users subscribers', 'wiki.ros.org users', 'answers users', 'discourse users']),
+    ('Wiki Stats', ['wiki pages', 'wiki page views']),
+    ('ROS Answers Stats', ['Questions', 'Answered Questions']),
 ]
-SINGLE_PLOTS = ['Total Downloads', 'Unique IPSs','Papers Citing']
+SINGLE_PLOTS = ['Total Downloads', 'Unique IPSs', 'Papers Citing']
 
 D = csv.DictReader(open('aggregated.csv'))
 ROWS = {}
@@ -23,12 +22,12 @@ for row in D:
         except:
             a.append(None)
     ROWS[row['']] = a
-    
-dates = [parser.parse(a) for a in D.fieldnames[1:]]
+
+dates = [parser.parse(d) for d in D.fieldnames[1:]]
 for name, keys in PLOTS:
     for key in keys:
         plot(dates, ROWS[key], 'o-', label=key)
-    legend()
+    legend(loc=0)
     title(name)
     show()
 for key in SINGLE_PLOTS:
