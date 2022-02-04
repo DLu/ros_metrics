c80258756412ff5bb437c1fccec6822d724d8f1b
David V. Lu <davidvlu@gmail.com>
2022-02-03 22:40:27 -0500

    Improve Link Parsing

--- ros_metrics/link_analysis.py
+++ ros_metrics/link_analysis.py
@@ -3,19 +3,21 @@ import re
 from .constants import architectures, os_list
 
 
-APT_REPO_PREFIX = re.compile(r'^(ros[\-a-z23]*)/ubuntu/(.*)')
+APT_REPO_PREFIX = re.compile(r'^(ros[\-a-z23]*)/+(ubuntu|rhel)/(.*)')
 distro = r'(' + '|'.join(os_list) + ')'
 DISTRO_PREFIX = re.compile(r'^dists/' + distro + r'/(.*)')
 POOL_PREFIX = re.compile(r'^pool/main/./[^/]+/(.*)')
+RHEL_PREFIX = re.compile(r'8/x86_64/Packages/./(.*)')
 arches = '|'.join(architectures)
 arch = r'(?:binary-(' + arches + ')|(source))'
 short_arch = r'(' + arches + '|all)'
 version = r'([\d\-\.~]+)'
 ROS_PKG_PREFIX = re.compile(r'^ros\-(\w+)\-([^_]+)_' + version + distro + '(.*)')
+ROS_RPM_PREFIX = re.compile(r'^ros\-(\w+)\-([\w\-]+)\-' + version + r'\.(el8)\.(.*)')
 ARCH_LIBRARY_PREFIX = re.compile(r'([^_]+)_.+_' + short_arch + '(.*)$')
 LIBRARY_PREFIX = re.compile(r'([^_]+)_' + version + r'(\..*)$')
 
-PACKAGE_SUFFIX = re.compile(r'([^/]*\.deb|\.dsc|\.debian\.tar\.[xg]z|\.orig\.tar\.gz)$')
+PACKAGE_SUFFIX = re.compile(r'([^/]*\.deb|\.dsc|\.debian\.tar\.[xg]z|\.orig\.tar\.gz|[^/]+\.rpm)$')
 
 ARCH_INFO = re.compile(r'main/' + arch + '/(Packages(.gz)?|Release|Sources.gz)$')
 META_INFO = re.compile(r'(In)?Release(\.gpg)?$')
@@ -31,7 +33,7 @@ def classify_link(s):
     m = APT_REPO_PREFIX.match(s)
     if not m:
         return
-    data['apt_repo'], s = m.groups()
+    data['apt_repo'], os, s = m.groups()
 
     m = DISTRO_PREFIX.match(s)
     if m:
@@ -45,12 +47,19 @@ def classify_link(s):
         else:
             return None
     m = POOL_PREFIX.match(s)
-    if not m:
-        return None
+    if m:
+        s = m.group(1)
+    else:
+        m = RHEL_PREFIX.match(s)
+        if not m:
+            return None
+        s = m.group(1)
 
     s = m.group(1)
-    m = ROS_PKG_PREFIX.match(s)
-    if m:
+    m1 = ROS_PKG_PREFIX.match(s)
+    m2 = ROS_RPM_PREFIX.match(s)
+    if m1 or m2:
+        m = m1 or m2
         # Ignore the version
         data['rosdistro'], data['package'], version, data['distro'], s = m.groups()
         if PACKAGE_SUFFIX.match(s):
